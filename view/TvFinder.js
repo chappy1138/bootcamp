f30b23babd46623d57d8fb81307de90beeccf79c
Orren <ochapman@walmart.com>
2013-03-14 07:56:53 -0700

    even more refactoring, again

--- view/TvFinder.js
+++ view/TvFinder.js
@@ -7,9 +7,16 @@ define([
     function ($, Backbone, SingleColumnTemplate, SiteHeader, Greeting) {
         return Backbone.View.extend({
             initialize: function () {
+                // create slots
                 new SingleColumnTemplate({ el: $('body'), model: this.model }).render();
-                new SiteHeader({ model: this.model }).render().appendTo('.appHeader');
-                new Greeting({ model: this.model }).render().appendTo('.appBody');
+
+                // populate slots
+                this.options.views = [];
+                this.options.views.push(new SiteHeader({ model: this.model }).appendTo('.appHeader'));
+                this.options.views.push(new Greeting({ model: this.model }).appendTo('.appBody'));
+            },
+            render: function () {
+                this.options.views.forEach(function (view) { view.render(); });
             }
         });
     }
