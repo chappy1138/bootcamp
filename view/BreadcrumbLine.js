4d6585e2b7060b495b550cc99c8f525800517a23
Orren <ochapman@walmart.com>
2013-03-16 22:57:14 -0700

    add environment support

--- view/BreadcrumbLine.js
+++ view/BreadcrumbLine.js
@@ -1,4 +1,13 @@
-define(['view/Base', 'handlebars', 'underscore'], function (BaseView, Handlebars, _) {
+define(['view/Base', 'handlebars', 'underscore', 'environment'], function (BaseView, Handlebars, _, environment) {
+        function makeAbsolute(href, isSecure) {
+            if (/^\//.test(href)) {
+                console.log(((isSecure) ? environment.secure_www : environment.www) + href);
+                return ((isSecure) ? environment.secure_www : environment.www) + href;
+            }
+            else {
+                return href;
+            }
+        }
         return BaseView.extend({
                 name: 'BreadcrumbLine',
                 tagName: "ul",
@@ -14,23 +23,31 @@ define(['view/Base', 'handlebars', 'underscore'], function (BaseView, Handlebars
                     if (attributes.home) {
                         crumb = _.extend({
                                 title: attributes.home.name,
-                                className: "breadcrumbHome"
+                                className: "breadcrumbHome",
+                                isSecure: false
                             },
-                            attributes.home
+                            attributes.home,
+                            {
+                                href: makeAbsolute(attributes.home.href, attributes.home.isSecure)
+                            }
                         );
                         crumbs.push(crumb);
                     }
                     for (index = 0; index < attributes.crumbs.length; index++) {
                         crumb = _.extend({
                                 title: attributes.crumbs[index].name,
-                                className: "breadcrumb"
+                                className: "breadcrumb",
+                                isSecure: false
                             },
-                            attributes.crumbs[index]
+                            attributes.crumbs[index],
+                            {
+                                href: makeAbsolute(attributes.crumbs[index].href, attributes.crumbs[index].isSecure)
+                            }
                         );
                         crumbs.push(crumb);
                     }
                     content = template(crumbs);
-                    this.$el.html(content);
+                    this.html(content);
                     return this;
 
                 }
