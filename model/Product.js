1c741ade66db0cdece96a7f30c698211422fb613
Orren <ochapman@walmart.com>
2013-03-25 12:08:05 -0700

    support simple http access to target/index.html

--- model/Product.js
+++ model/Product.js
@@ -1,12 +1,17 @@
 define(['Backbone'], function (Backbone) {
         var superclass = Backbone.Model;
         return superclass.extend({
-                urlRoot: '/product',
                 idAttribute: 'base_item_id',
                 parse: function (response) {
                     return {
                         items: response
                     }
+                },
+                sync: function (method, model, options) {
+                    if (method === 'read') {
+                        model.url = '../json/product/item-' + model.get('base_item_id') + '.json';
+                    }
+                    return superclass.prototype.sync.apply(this, arguments);
                 }
             }
         );
--- model/Review.js
+++ model/Review.js
@@ -1,12 +1,18 @@
 define(['Backbone'], function (Backbone) {
         var superclass = Backbone.Model;
         return superclass.extend({
-                urlRoot: '/review',
+                urlRoot: '../json/review',
                 idAttribute: 'base_item_id',
                 parse: function (response) {
                     return {
                         entries: response
                     }
+                },
+                sync: function (method, model, options) {
+                    if (method === 'read') {
+                        model.url = '../json/reviews.json';
+                    }
+                    return superclass.prototype.sync.apply(this, arguments);
                 }
             }
         );
